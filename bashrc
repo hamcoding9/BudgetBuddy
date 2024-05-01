3c9e47448116403a2f7120864a7e23da411c946e
hamcoding9 <dientes8520@gmail.com>
2024-05-01 12:41:12 +0000

    add: POST Method로 테스트 함수 변경

---
+++ bashrc
@@ -0,0 +1,1 @@
+export PYTHONPATH=$PYTHONPATH:/usr/local/lib64/python3.9/site-packages
--- main.py
+++ main.py
@@ -10,3 +10,8 @@ def read_root() -> Union[str, dict]:
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None) -> dict:
     return {"item_id": item_id, "q": q}
+
+# 카카오 챗봇의 기본 METHOD는 HTTP POST
+@app.post("/hello")
+def hello_world():
+    return {"Hello": "World"}
