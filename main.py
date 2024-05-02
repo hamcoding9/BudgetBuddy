237d417bc3d96d9ff3f4304af4c6d314007c72b3
hamcoding9 <dientes8520@gmail.com>
2024-05-02 18:52:18 +0900

    add: 기본 문자열 반환 함수 테스트

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import Union
 from fastapi import FastAPI
+import random
 
 app = FastAPI()
 
@@ -15,3 +16,35 @@ def read_item(item_id: int, q: str = None) -> dict:
 @app.post("/hello")
 def hello_world():
     return {"Hello": "World"}
+
+# 랜덤 문자열 선택 함수
+def get_random_string():
+    strings = [
+        "영아기", "유아기", "초등학생", "중학생", 
+        "고등학생", "대학생", "석사생", "박사생", 
+        "사원", "대리", "과장"
+    ]
+    return random.choice(strings)
+
+# 기본 문자열 반환 함수 테스트
+@app.post("/random")
+def random_():
+    # 랜덤 문자열 선택
+    random_string = "당신의 지출 관리 레벨은 " + get_random_string() + "입니다."
+    
+    # JSON 형식의 응답 생성
+    response_data = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": random_string
+                    }
+                }
+            ]
+        }
+    }
+    
+    # JSON 응답 반환
+    return response_data
