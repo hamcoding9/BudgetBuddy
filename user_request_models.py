a8652d4b3ea2fed54ded68c3bffd78e36c45b400
hamcoding9 <dientes8520@gmail.com>
2024-05-08 20:35:23 +0900

    add: 사용자 id 반환 함수 추가

--- user_request_models.py
+++ user_request_models.py
@@ -1,15 +1,20 @@
 from pydantic import BaseModel
 
+class Action(BaseModel):
+    name: str
+    
+class User(BaseModel):
+    id: str
+    type: str
+    properties: dict
+
 class UserRequest(BaseModel):
     timezone: str
     params: dict
     block: dict
     utterance: str
     lang: str
-    user: dict
-
-class Action(BaseModel):
-    name: str
+    user: User
 
 class RequestPayload(BaseModel):
     userRequest: UserRequest
