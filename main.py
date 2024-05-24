4b4a0c11599411657e22a729050e28faff377e6b
hamcoding9 <dientes8520@gmail.com>
2024-05-24 20:24:49 +0900

    bbd와 합체

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import json
 from typing import Union
 from fastapi import FastAPI, Request
 import utils.user_request_models as user_request_models
 import utils.generator_utils as generator
+from bbd import worker
+import os
 
 app = FastAPI()
 
@@ -12,9 +15,27 @@ async def process_utterance(request: Request):
     payload = await request.json()
     request_payload = user_request_models.RequestPayload(**payload)
     utterance = request_payload.userRequest.utterance
+    user_id = request_payload.userRequest.user.id
 
+    task = "record"
+    user_request = {
+        "user_id": user_id,
+        "user_text": utterance
+    }
+    bbd_worker = worker(task, **user_request)
+    user_path = "/home/ec2-user/user_files"
+    with open(os.path.join(user_path, "user_info.json"), "w") as f:
+        user_json = json.load(f)
+    user_json[user_id]["api_key"] = os.path.join(user_path, user_json[user_id]["api_key"])
+
+    output = bbd_worker(user_json)
+
+    if output.trial:
+        response_string  = "등록 완료되었습니다. 버버두두!"
+    else:
+        response_string = "등록 실패했습니다. 버버두두!"
+    
     # json 형식의 응답 생성
-    response_string = utterance + "\n등록 완료되었습니다. 버버두두!"
     return generator.generate_chat(response_string)
 
 # 사용자 id 반환
